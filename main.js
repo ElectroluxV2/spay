1d744ae44df8f307e5cfac747ab0259fef8eff20
ElectroluxV2 <electroluxv2@gmail.com>
2021-07-31 22:57:19 +0200

    Updated example usage

--- main.js
+++ main.js
@@ -42,14 +42,34 @@ function drawHex(layout, hexagon) {
 
 const layout = new Layout(Orientation.FLAT, new Point(20, 20), new Point(500, 500));
 
-drawHex(layout, new Hexagon(-1, -1, -1));
-drawHex(layout, new Hexagon(0, 0, -1));
-drawHex(layout, new Hexagon(0, -1, 0));
-drawHex(layout, new Hexagon(-1, 0, 0));
-drawHex(layout, new Hexagon(0, 0, 0));
-drawHex(layout, new Hexagon(1, 0, 0));
-drawHex(layout, new Hexagon(0, 1, 0));
-drawHex(layout, new Hexagon(0, 0, 1));
-drawHex(layout, new Hexagon(1, 1, 1));
+window.onpointermove = ({pageX, pageY}) => {
+    const point = new Point(pageX, pageY);
+    layout.origin = point;
+}
+
+let zoom = 20;
+
+window.onwheel = ({deltaY}) => {
+    zoom -= Math.sign(deltaY);
+    layout.size.x = zoom;
+    layout.size.y = zoom;
+}
+
+function loop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let q = -5; q < 5; q++) {
+        for (let r = -5; r < 5; r++) {
+            for (let s = -5; s < 5; s++) {
+                const hexagon = new Hexagon(q, r, s);
+                drawHex(layout, hexagon);
+            }
+        }
+    }
+
+    window.requestAnimationFrame(loop);
+}
+
+loop();
 
 
